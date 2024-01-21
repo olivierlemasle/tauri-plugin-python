1d58e132df057fdb82edc8ccbaefe208a196da2c
Olivier Lemasle <olivier.lemasle@apalia.net>
2024-01-21 20:45:58 +0100

    Format + remove mobile

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,11 @@
 use tauri::{
-  plugin::{Builder, TauriPlugin},
-  Manager, Runtime,
+    plugin::{Builder, TauriPlugin},
+    AppHandle, Manager, Runtime,
 };
 
 use std::{collections::HashMap, sync::Mutex};
 
 pub use models::*;
-
-#[cfg(desktop)]
-mod desktop;
-#[cfg(mobile)]
-mod mobile;
 
 mod commands;
 mod error;
@@ -18,39 +13,41 @@ mod models;
 
 pub use error::{Error, Result};
 
-#[cfg(desktop)]
-use desktop::Python;
-#[cfg(mobile)]
-use mobile::Python;
+pub struct Python<R: Runtime>(AppHandle<R>);
+
+impl<R: Runtime> Python<R> {
+    pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
+        Ok(PingResponse {
+            value: payload.value,
+        })
+    }
+}
 
 #[derive(Default)]
 struct MyState(Mutex<HashMap<String, String>>);
 
 /// Extensions to [`tauri::App`], [`tauri::AppHandle`] and [`tauri::Window`] to access the python APIs.
 pub trait PythonExt<R: Runtime> {
-  fn python(&self) -> &Python<R>;
+    fn python(&self) -> &Python<R>;
 }
 
 impl<R: Runtime, T: Manager<R>> crate::PythonExt<R> for T {
-  fn python(&self) -> &Python<R> {
-    self.state::<Python<R>>().inner()
-  }
+    fn python(&self) -> &Python<R> {
+        self.state::<Python<R>>().inner()
+    }
 }
 
 /// Initializes the plugin.
 pub fn init<R: Runtime>() -> TauriPlugin<R> {
-  Builder::new("python")
-    .invoke_handler(tauri::generate_handler![commands::execute])
-    .setup(|app, api| {
-      #[cfg(mobile)]
-      let python = mobile::init(app, api)?;
-      #[cfg(desktop)]
-      let python = desktop::init(app, api)?;
-      app.manage(python);
+    Builder::new("python")
+        .invoke_handler(tauri::generate_handler![commands::execute])
+        .setup(|app, _api| {
+            let python = Python(app.clone());
+            app.manage(python);
 
-      // manage state so it is accessible by the commands
-      app.manage(MyState::default());
-      Ok(())
-    })
-    .build()
+            // manage state so it is accessible by the commands
+            app.manage(MyState::default());
+            Ok(())
+        })
+        .build()
 }
