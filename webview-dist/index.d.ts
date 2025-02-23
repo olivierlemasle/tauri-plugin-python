a4d4989ac6bd549985c92df01499858e46d0da1d
Olivier Lemasle <olivier.lemasle@apalia.net>
2025-02-23 12:08:12 +0100

    Update Tauri - Refactor guest-js (#25)

--- webview-dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function addResourcePathToSysPath(path: string): Promise<void>;
-export declare function importModule(moduleName: string): Promise<void>;
-export declare function callFunction(moduleName: string, functionName: string, args: Array<any>): Promise<any>;
